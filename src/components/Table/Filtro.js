719f1f71080d79faba9fa583d6a751e61d1cfe20
Lucas Cisternas <l.cisternasopazo@gmail.com>
2021-11-29 17:32:07 -0300

    filtros listos

--- src/components/Table/Filtro.js
+++ src/components/Table/Filtro.js
@@ -1,32 +1,35 @@
 import React, { useState, Fragment } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ContenedorFiltro ,BotonFiltro } from './style';
 import { FiltroPendientes } from '../../state/actions/restaurantActions';
 import { FiltroVisitados } from '../../state/actions/restaurantActions';
+import { AllRestaurant } from '../../state/actions/restaurantActions';
 
 const Filtro = () => {
 
     const [show, setShow] = useState(false)
     const dispatch = useDispatch()
+    const token = "Token " + useSelector(state=> state.auth.token)
     const pendientes = () => dispatch( FiltroPendientes() )
     const visitados = () => dispatch( FiltroVisitados() )
+    const restaurantes = (info) => dispatch( AllRestaurant(info) ) 
 
     return (
         <ContenedorFiltro>
             {show ? (
                 <BotonFiltro
                     type='button'
-                    onClick={() => {setShow(false)}}
+                    onClick={() => (restaurantes(token), setShow(false))}
                 >Quitar filtro</BotonFiltro>
             ) : (
                 <Fragment>
                     <BotonFiltro
                         type='button'
-                        onClick={() => {visitados(), setShow(true)}}
+                        onClick={() => (visitados(), setShow(true))}
                     >Visitados</BotonFiltro>
                     <BotonFiltro
                         type='button'
-                        onClick={() => {pendientes(), setShow(true)}}
+                        onClick={() => (pendientes(), setShow(true))}
                     >Pendientes</BotonFiltro>
                 </Fragment>
             )}
